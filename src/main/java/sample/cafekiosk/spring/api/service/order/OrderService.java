ec821cf7991a7235feef961a94b1069fa672541b
조경훈 <kh.cho@okestro.com>
2024-04-24 17:34:25 +0900

    Business Layer 테스트 코드 작성

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -1,37 +1,90 @@
 package sample.cafekiosk.spring.api.service.order;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.cafekiosk.spring.api.domain.order.Order;
 import sample.cafekiosk.spring.api.domain.order.OrderRepository;
 import sample.cafekiosk.spring.api.domain.product.Product;
 import sample.cafekiosk.spring.api.domain.product.ProductRepository;
+import sample.cafekiosk.spring.api.domain.product.ProductType;
+import sample.cafekiosk.spring.api.domain.stock.Stock;
+import sample.cafekiosk.spring.api.domain.stock.StockRepository;
 import sample.cafekiosk.spring.api.dto.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
+    private final StockRepository stockRepository;
 
-    public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registerDateTime) {
+    /**
+     * 재고 감소 -> 동시성 고민
+     * optimistic lock / pessimistick lock / ...
+     * 수량으로 우선권 혹은 시간으로 우선권
+     */
 
-
+    public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registerDateTime){
         List<String> productNumbers = request.getProductNumbers();
         // Product
         List<Product> products = findProductsBy(productNumbers);
+
+        deductToQuantities(products);
+
 
         // Order
         Order order = Order.create(products, registerDateTime);
         Order savedOrder = orderRepository.save(order);
         return OrderResponse.of(savedOrder);
+    }
+
+    private void deductToQuantities(List<Product> products) {
+        // 재고 차감 체크가 필요한 filter
+        List<String> stockProductNumbers = extractStockProductNumbers(products);
+
+        // 재고 엔티티 조회
+        Map<String, Stock> stockMap = createStockMapBy(stockProductNumbers);
+
+        // 상품별 counting
+        Map<String, Long> productCountingMap = createCountingMapBy(stockProductNumbers);
+
+        // 재고 차감 시도
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+            if(stock.isQuantityLessThan(quantity)){
+              throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            stock.deductQuantity(quantity);
+        }
+    }
+
+    private static List<String> extractStockProductNumbers(List<Product> products) {
+        return products.stream()
+            .filter(product -> ProductType.containsStockType(product.getType()))
+            .map(Product::getProductNumber)
+            .toList();
+    }
+
+    private Map<String, Stock> createStockMapBy(List<String> stockProductNumbers) {
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        return stocks.stream()
+            .collect(Collectors.toMap(Stock::getProductNumber, stock -> stock));
+    }
+
+    private static Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
+        return stockProductNumbers.stream()
+            .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
     }
 
     private List<Product> findProductsBy(List<String> productNumbers) {
